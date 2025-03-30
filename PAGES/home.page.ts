bb445b594f4d9c871aa76e745b0cab2413e5e327
caromerou <caromeroulloa@hotmail.com>
2025-03-30 09:31:39 -0500

    Añadir carpeta PAGES con scripts de automatización

---
+++ PAGES/home.page.ts
@@ -0,0 +1,19 @@
+import { Page, Locator } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+  readonly profileIcon: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.profileIcon = page.locator("ul > li.nav-account > a");
+  }
+
+  async clickProfileIcon() {
+    await this.profileIcon.click();
+  }
+
+  async goto() {
+    await this.page.goto('https://playwright.dev/');
+  }
+}git add .
